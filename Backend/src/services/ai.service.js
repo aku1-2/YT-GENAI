490fd0129e0547fc23dd6adc15143de72d3929ec
Akanksha <168823584+aku1-2@users.noreply.github.com>
2026-05-08 00:01:31 +0530

    Fix content string formatting in invokeGeminiAI

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -5,7 +5,7 @@ const ai = new GoogleGenAI({ apiKey: process.env.Google_GENAI_API_KEY });
 async function invokeGeminiAI(){
     const response = await ai.models.generateContent({
         model: "gemini-2.5-flash",
-        contents : "Hello gemini ! Explain what is Interview ?"
+        contents: "Hello gemini! Explain what an interview is?"
     })
 
     console.log(response.text)
